4eaafc92a4c95595e653008e4d662aab4d0842b5
naagaraa <ekabersinar@gmail.com>
2025-06-15 22:44:58 +0700

    update note ke notion

---
+++ app/apps/pdf/image-to-pdf/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return <div>image to pdf</div>;
+}
+export const metadata = {
+  title: "Calorie Calculator",
+  description: "Calculate your daily calorie needs and track your intake.",
+};
