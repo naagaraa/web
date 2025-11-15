28f8bcd53e9f7102cddcaf46630f98f95bad7352
naagaraa <ekabersinar@gmail.com>
2025-11-15 12:59:54 +0700

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "react-hot-toast";
 import "./globals.css";
 import { Hanken_Grotesk } from "next/font/google";
 import InAppBrowserWarning from "../components/InAppBrowserWarning";
+import Script from "next/script";
 
 const hanken = Hanken_Grotesk({ weight: "400", subsets: ["latin"] });
 
@@ -12,6 +13,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={hanken.className}>
+      <Script
+        async
+        src="{`https://www.googletagmanager.com/gtag/js?id=G-50EDMDZV2X`}"
+        strategy="afterInteractive"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-50EDMDZV2X');
+       `}
+      </Script>
       <body>
         {children}
         <Toaster position="top-center" />
